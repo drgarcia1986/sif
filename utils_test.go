fa3f810f7c7b42e8336fc1b20a108642e7a8a526
Diego Garcia <drgarcia1986@gmail.com>
2017-03-02 23:21:46 -0300

    add tests :tada:

---
+++ utils_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestIsBinary(t *testing.T) {
+	var isBinaryTests = []struct {
+		filename string
+		expected bool
+	}{
+		{"python.txt", false},
+		{"binary_file", true},
+	}
+
+	for _, tc := range isBinaryTests {
+		path := fmt.Sprintf("./_tests/%s", tc.filename)
+		file, err := os.Open(path)
+		if err != nil {
+			t.Fatalf("Error on open file %s", err)
+		}
+		defer file.Close()
+
+		actual, err := isBinary(file)
+		if err != nil {
+			t.Fatalf("Error on check file %s", err)
+		}
+
+		if actual != tc.expected {
+			t.Errorf("expected %s, got %s", tc.expected, actual)
+		}
+	}
+}
