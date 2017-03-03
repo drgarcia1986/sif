acf15fc5cfb18facedce0496fb8f5ba836df6292
Diego Garcia <drgarcia1986@gmail.com>
2017-03-02 22:25:06 -0300

    break sif brain in more files sif for main struct and main funcs utils for some utils like `isBinary` func main for command line interface

--- main.go
+++ main.go
@@ -1,35 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"path/filepath"
-	"regexp"
 
 	"github.com/fatih/color"
-)
-
-type Match struct {
-	Line int
-	Text string
-}
-
-type FileMatched struct {
-	Name    string
-	Matches []Match
-}
-
-var (
-	colorBgYellow = color.New(color.BgYellow, color.FgBlack).SprintFunc()
-	bgYellow      = func(s string) string { return colorBgYellow(s) }
-	yellow        = color.New(color.Bold, color.FgYellow).SprintFunc()
-	green         = color.New(color.Bold, color.FgGreen)
-	ignoreDirs    = regexp.MustCompile(`\.(svn|git*)`)
 )
 
 func main() {
@@ -41,8 +17,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	pattern := regexp.MustCompile(fmt.Sprintf("(?i)%s", args[0]))
-
 	dirs := make([]string, 0)
 	if len(args) < 2 {
 		dirs = append(dirs, "./")
@@ -50,9 +24,10 @@ func main() {
 		dirs = append(dirs, args[1:]...)
 	}
 
+	s := New(args[0])
 	files := make([]*FileMatched, 0)
 	for _, dir := range dirs {
-		fs, err := scanDir(dir, pattern)
+		fs, err := s.ScanDir(dir)
 		if err != nil {
 			fmt.Printf("error to search for pattern: %s", err)
 			os.Exit(1)
@@ -60,6 +35,8 @@ func main() {
 		files = append(files, fs...)
 	}
 
+	green := color.New(color.Bold, color.FgGreen)
+	yellow := color.New(color.Bold, color.FgYellow).SprintFunc()
 	for i, f := range files {
 		green.Println(f.Name)
 		for _, match := range f.Matches {
@@ -69,73 +46,4 @@ func main() {
 			fmt.Println()
 		}
 	}
-}
-
-func scanDir(dir string, pattern *regexp.Regexp) ([]*FileMatched, error) {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	filesMatched := make([]*FileMatched, 0)
-	for _, f := range files {
-		path := filepath.Join(dir, f.Name())
-		if !f.IsDir() {
-			fm, err := scanFile(path, pattern)
-			if err != nil {
-				return nil, err
-			}
-			if fm != nil {
-				filesMatched = append(filesMatched, fm)
-			}
-		} else if !ignoreDirs.MatchString(f.Name()) {
-			fs, err := scanDir(path, pattern)
-			if err != nil {
-				return nil, err
-			}
-			filesMatched = append(filesMatched, fs...)
-		}
-	}
-
-	return filesMatched, nil
-}
-
-func scanFile(path string, pattern *regexp.Regexp) (*FileMatched, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	if b, err := isBinary(file); b || err != nil {
-		return nil, err
-	}
-
-	line := 1
-	matches := make([]Match, 0)
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		text := scanner.Text()
-		if pattern.MatchString(text) {
-			text := pattern.ReplaceAllStringFunc(text, bgYellow)
-			matches = append(matches, Match{line, text})
-		}
-		line++
-	}
-
-	if len(matches) > 0 {
-		return &FileMatched{path, matches}, nil
-	}
-
-	return nil, nil
-}
-
-func isBinary(file *os.File) (bool, error) {
-	buffer := make([]byte, 512)
-	n, err := file.Read(buffer)
-	if err != nil && err != io.EOF {
-		return true, err
-	}
-	file.Seek(0, 0)
-	return http.DetectContentType(buffer[:n]) == "application/octet-stream", nil
 }
